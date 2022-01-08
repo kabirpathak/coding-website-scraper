19da091c88447859379eb21d7280508dc36dd918
kabirpathak <kabirpathak99@gmail.com>
2022-01-08 20:51:26 +0530

    Basic outlook

---
+++ scrape.js
@@ -0,0 +1,60 @@
+const PORT = 8000
+const axios = require('axios')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+const codechef_url = 'https://www.codechef.com/users/kabirpathak';
+const hackerrank_url = 'https://www.hackerrank.com/rest/hackers/TARUN_V_REDDY/rating_histories_elo';
+const hackerearth_url = 'https://www.hackerearth.com/@prashantpokhriyal/';
+
+
+exports.scrape = function(url, website) {
+    let current_rating = 0, max_rating = 0;
+    axios(url, {headers: { 'User-Agent':'firefox' }})
+    .then(response => {
+        switch(website) {
+            case 'hackerrank':
+                const $hr = response.data.models;
+                list = [];
+                for(let i = 0;i < $hr.length;i++) {
+                    if($hr[i].category == 'Algorithms') list = $hr[i].events;
+                }
+
+                for(let i = 0;i < list.length;i++) {
+                    current_rating = list[i].rating;
+                    if(current_rating > max_rating)max_rating = current_rating;
+                }
+            break;
+
+            case 'codechef':
+                const $ = cheerio.load(response.data);
+                $('.rating-number', response.data).each(function() {
+                current_rating = $(this).text();
+                })
+            break;
+
+            case 'codeforces':
+                const $cf = cheerio.load(response.data);
+                $('.')
+            break;
+        }
+
+        //hackerearth
+        // const html = response.data;
+        // console.log(html);
+        // const $ = cheerio.load(html);
+        // // console.log($);
+        // $('.contest-ratings.item', html).each(function() {
+        //     console.log('inside');
+        // })
+        // // console.log(current_rating);
+        // // console.log(max_rating);
+
+        console.log(current_rating);
+
+        return [current_rating, max_rating];
+    }).catch(err => console.log(err));
+}
+
+app.listen(PORT, () => console.log(`server running on port ${PORT}`))
